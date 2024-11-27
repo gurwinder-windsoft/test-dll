b14d93f077d67fdb3e6e0bc1cdf0bc3351226a83
gurwinder-windsoft <162701704+gurwinder-windsoft@users.noreply.github.com>
2024-11-27 13:16:44 +0530

    Update mongo.ps1

--- mongo.ps1
+++ mongo.ps1
@@ -16,11 +16,13 @@ function Export-PreprodData {
     try {
         $mongoExportCommand = "& 'C:\mongodb-tools\mongodb-database-tools-windows-x86_64-100.10.0\bin\mongoexport.exe' --uri=`"$preprodUri`" --db=SyncNotifyHubService --collection=$collectionName --out=`"$exportPath`" --jsonArray --authenticationDatabase=admin"
         Write-Host "Exporting $collectionName from preprod..."
+        Write-Host "Running export command: $mongoExportCommand"
         Invoke-Expression $mongoExportCommand
         Write-Host "Exported $collectionName successfully to $exportPath"
         return $exportPath
     } catch {
         Write-Host "Error exporting data from preprod: $($_.Exception.Message)"
+        Write-Host "StackTrace: $($_.Exception.StackTrace)"
         return $null
     }
 }
@@ -36,6 +38,7 @@ function Import-ProdData {
         if (Test-Path $exportFile) {
             $importCommand = "mongoimport --uri=$prodUri --db=SyncNotifyHubService --collection=$collectionName --file=$exportFile --jsonArray --authenticationDatabase=admin --upsert"
             Write-Host "Importing data into prod from $exportFile..."
+            Write-Host "Running import command: $importCommand"
             Invoke-Expression $importCommand
             Write-Host "Data imported into prod successfully."
         } else {
@@ -51,7 +54,7 @@ $collections = @("Client", "User")  # List of collections to export/import
 
 foreach ($collectionName in $collections) {
     # Step 1: Export data from preprod
-    $exportFile = Export-PreprodData -collectionName $collectionName -exportPath $env:GITHUB_WORKSPACE\$collectionName.json
+    $exportFile = Export-PreprodData -collectionName $collectionName -exportPath "$env:GITHUB_WORKSPACE\$collectionName.json"
     if ($exportFile) {
         # Step 2: Import data into prod
         Import-ProdData -collectionName $collectionName -exportFile $exportFile
