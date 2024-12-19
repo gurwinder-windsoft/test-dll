9db2ddc6ccb0c18e0675faf84eebe2344cfebd68
gurwinder-windsoft <162701704+gurwinder-windsoft@users.noreply.github.com>
2024-12-19 16:28:44 +0530

    Update mongo.ps1

--- mongo.ps1
+++ mongo.ps1
@@ -1,6 +1,6 @@
 # Define MongoDB URIs
 $preprodURI = "mongodb://preprodadmin:adminPPDSNM482@40.113.40.126:27017/?authSource=admin"
-$prodURI = "mongodb://adminintuser:adminYBXdPS274@68.219.243.214:27022/?authSource=admin"
+$prodURI = "mongodb://admin:adminYBXdGH123@68.219.243.214:27017/?authSource=admin"
 
 # Define collections to export and import
 $collections = @("Client", "User")
@@ -14,11 +14,11 @@ foreach ($collectionName in $collections) {
 
   # Step 4: Export collection data from preprod to a file
   Write-Host "Exporting $collectionName data from preprod"
-  & 'C:\mongodb-tools\mongodb-database-tools-windows-x86_64-100.10.0\bin\mongoexport.exe' --uri=$preprodURI --db=SyncNotifyHubService --collection=$collectionName --out=$collectionExportPath --jsonArray
+  & 'C:\mongodb-tools\mongodb-database-tools-windows-x86_64-100.10.0\bin\mongoexport.exe' --uri=$prodURI --db=SyncNotifyHubService --collection=$collectionName --out=$collectionExportPath --jsonArray
   Write-Host "Exported $collectionName data to $collectionExportPath"
 
   # Step 5: Import collection data into prod
   Write-Host "Importing $collectionName data into prod"
-  & 'C:\mongodb-tools\mongodb-database-tools-windows-x86_64-100.10.0\bin\mongoimport.exe' --uri=$prodURI --db=SyncNotifyHubService --collection=$collectionName --file=$collectionExportPath --jsonArray --upsert --verbose 
+  & 'C:\mongodb-tools\mongodb-database-tools-windows-x86_64-100.10.0\bin\mongoimport.exe' --uri=$preprodURI --db=SyncNotifyHubService --collection=$collectionName --file=$collectionExportPath --jsonArray --upsert --verbose 
   Write-Host "Imported $collectionName data into prod"
 }
